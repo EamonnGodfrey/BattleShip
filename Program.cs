4b831f51ffdc9606c364fb5bd451a22cc353fa69
EamonnGodfrey <eamonn.godfrey@gmail.com>
2020-10-01 20:46:06 +1000

    refactored code to store gameboard in 2d array instead of a 1d array lol

--- Program.cs
+++ Program.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BattleShip
 {
     class GameBoard
     {
-        public static string[] _initialBoardState;
-        public static string[] _boardPositionalMarkers;
-        public string[] _boardLayout;
+        public static string[,] _initialBoardState;
+        public static string[,] _boardPositionalMarkers;
+        public string[,] _boardLayout;
 
     }
 
@@ -28,36 +24,50 @@ namespace BattleShip
         static void Main(string[] args)
         {
             GameBoard playerBoard;
+            int gridSize = 5;
+            gridSize += 1;
             //DisplayBoard playerDisplay;
 
-            playerBoard = InitialisePlayerBoard(); //Initialising gameboard state
-            InitialisePositionalMarkers();
-
-            PrintPlayerBoard(playerBoard); //Displaying current board state.
-
-            PlaceTokens(playerBoard);
+            playerBoard = InitialisePlayerBoard(gridSize);   //Initialising gameboard state
+            InitialisePositionalMarkers(gridSize);          //InitialisePositionalMarkers();
+            
+            PrintPlayerBoard(playerBoard, gridSize);      //Displaying current board state.
 
             
+            PlaceTokens(playerBoard, gridSize);
+
+
 
 
         }
         // ...........................MAIN.........................
 
-        static void PlaceTokens(GameBoard inGameBoard)
+        static void PlaceTokens(GameBoard inGameBoard, int inputGridSize)
         {
-            for (int i = 0; i < 4; i++)
+            for (int turn = 0; turn < 4; turn++)
             {
                 string inputCoord, checkedCoord;
                 bool isValid = false;
                 Console.Write("\nEnter grid co-ordinates to place token: ");
                 inputCoord = Console.ReadLine().ToUpper();
-                for (int x = 0; x < GameBoard._boardPositionalMarkers.Length; x++)
+                for (int y = 0; y < inputGridSize; y++)
                 {
-                    checkedCoord = GameBoard._boardPositionalMarkers[x];
-                    isValid = (string.Equals(checkedCoord, inputCoord));
+                    for (int x = 0; x < inputGridSize; x++)
+                    {
+                        checkedCoord = GameBoard._boardPositionalMarkers[y, x];
+                        if (inputCoord == "0")
+                        {
+                            break;
+                        }
+                        isValid = (string.Equals(checkedCoord, inputCoord));
+                        if (isValid == true)
+                        {
+                            inGameBoard._boardLayout[y, x] = "V";
+                            break;
+                        }
+                    }
                     if (isValid == true)
                     {
-                        inGameBoard._boardLayout[x] = "V";
                         break;
                     }
                 }
@@ -65,74 +75,110 @@ namespace BattleShip
                 {
                     Console.WriteLine("\nError\nPlease enter a valid co-ordinate: ");
                     inputCoord = Console.ReadLine().ToUpper();
-                    for (int o = 0; o < GameBoard._boardPositionalMarkers.Length; o++)
+                    for (int y = 0; y < inputGridSize; y++)
                     {
-                        checkedCoord = GameBoard._boardPositionalMarkers[o];
-                        isValid = string.Equals(checkedCoord, inputCoord);
+                        for (int x = 0; x < inputGridSize; x++)
+                        {
+                            checkedCoord = GameBoard._boardPositionalMarkers[y, x];
+                            if (inputCoord == "0")
+                            {
+                                break;
+                            }
+                            isValid = (string.Equals(checkedCoord, inputCoord));
+                            if (isValid == true)
+                            {
+                                inGameBoard._boardLayout[y, x] = "V";
+                                break;
+                            }
+                        }
                         if (isValid == true)
                         {
-                            inGameBoard._boardLayout[o] = "V";
                             break;
                         }
                     }
                 }
-                PrintPlayerBoard(inGameBoard);
+                PrintPlayerBoard(inGameBoard, inputGridSize);
             }
         }
 
-        static void PrintPlayerBoard(GameBoard boardToDisplay)
+        static void PrintPlayerBoard(GameBoard boardToDisplay, int inputGridSize)
         {
             string tmp = "";
-            for (int x = 0; x < 5; x++)
+            for (int y = 0; y < inputGridSize; y++)
             {
                 tmp = "";
-                for(int i = 0 + (x * 5); i < 5 + (x * 5); i++)
+                for (int x = 0; x < inputGridSize; x++)
                 {
-                    tmp += " "+boardToDisplay._boardLayout[i];
+                    tmp += $" {boardToDisplay._boardLayout[y, x],2}";
                 }
                 Console.WriteLine(tmp);
             }
-            ////////////
+
         }
 
-        static GameBoard InitialisePlayerBoard()
+        static GameBoard InitialisePlayerBoard(int inputGridSize)
         {
-            GameBoard gameBoards = new GameBoard();
-            //Game board initialised to 5x5 grid
-            GameBoard._initialBoardState = new string[25];
-            for (int x = 0; x < 25; x++)
-            {
-                GameBoard._initialBoardState[x] = "*";
-            }
-            gameBoards._boardLayout = GameBoard._initialBoardState;
-            
+            GameBoard playerBoard = new GameBoard();
+            GameBoard._initialBoardState = new string[inputGridSize, inputGridSize];
 
-            
-                return gameBoards;
-            /////////////////////
-        }
-        static void InitialisePositionalMarkers()
-        {
-            GameBoard._boardPositionalMarkers = new string[25];
             char rank = 'A';
-            char file = '1';
-            string position = "";
-            for (int x = 0; x < 5; x++)
+            //char file = '1';
+
+            for (int y = 0; y < inputGridSize; y++)
             {
-                file = '1';
-                for (int i = 0 + (x * 5); i < 5 + (x * 5); i++)
+                for (int x = 0; x < inputGridSize; x++)
                 {
-                    position = "";
-                    position += char.ToString(rank);
-                    position += char.ToString(file);
-                    GameBoard._boardPositionalMarkers[i] = position;
-                    ++file;
+                    if (y == 0 && x == 0)
+                    {
+                        GameBoard._initialBoardState[y, x] = " ";
+                    }
+                    else if (y == 0 && x != 0)
+                    {
+                        GameBoard._initialBoardState[y, x] = Convert.ToString(x);
+                        //++file;
+                    }
+                    else if (y != 0 && x == 0)
+                    {
+                        GameBoard._initialBoardState[y, x] = char.ToString(rank);
+                        ++rank;
+                    }
+                    else
+                    {
+                        GameBoard._initialBoardState[y, x] = "*";
+                    }
+                }
+            }
+            playerBoard._boardLayout = GameBoard._initialBoardState;
+            return playerBoard;
+
+
+
+        }
+        static void InitialisePositionalMarkers(int inputGridSize)
+        {
+            GameBoard._boardPositionalMarkers = new string[inputGridSize, inputGridSize];
+            char rank = '@';
+            string position = "";
+
+            for (int y = 0; y < inputGridSize; y++)
+            {
+                for (int x = 0; x < inputGridSize; x++)
+                {
+                    if (y == 0 && x == 0 || y == 0 && x != 0 || y != 0 && x == 0)
+                    {
+                        GameBoard._boardPositionalMarkers[y, x] = "0";
+                    }
+                    else
+                    {
+                        position = "";
+                        position += char.ToString(rank);
+                        position += Convert.ToString(x);
+                        GameBoard._boardPositionalMarkers[y, x] = position;
+                    }
+                   
                 }
                 ++rank;
             }
-            ///////////////////
         }
-
-
     }
 }
